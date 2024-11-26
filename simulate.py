b72f5881c5f07bdba7b463dff07ac84146551918
shaikh1994 <shaikh95@live.com>
2024-11-26 16:35:39 +0800

    simulate add timer

--- simulate.py
+++ simulate.py
@@ -1,7 +1,16 @@
 import pybullet as p
+import time
 
-# Connect to PyBullet GUI
-physicsClient = p.connect(p.GUI)
 
-# Disconnect from PyBullet
+p.connect(p.GUI)
+
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+
+for i in range(1000):
+    p.stepSimulation()  
+    time.sleep(1 / 60)  
+    print(i)  
+
+
 p.disconnect()
